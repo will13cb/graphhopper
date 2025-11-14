839ab76f42e221785338bba2012464657ea5dc22
William Caron-Bastarache <will13cb@gmail.com>
2025-11-14 18:14:34 -0500

    re added tests

--- core/src/test/java/com/graphhopper/util/AngleCalcTest.java
+++ core/src/test/java/com/graphhopper/util/AngleCalcTest.java
@@ -18,6 +18,8 @@
 package com.graphhopper.util;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 
 /**
